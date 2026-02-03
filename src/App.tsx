06b0035beb6b3ffe208bd3ef8ac4140f191603ec
Jonas Jeronimo <jonas.jeronimo@live.com>
2026-02-03 07:22:50 -0400

    chore: setup inicial do projeto React

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div>
-      <h1>Industrial Dashboard</h1>
+       <h1>Industrial Dashboard</h1>
       <p>Projeto de estudo e portfólio em React</p>
     </div>
   )
